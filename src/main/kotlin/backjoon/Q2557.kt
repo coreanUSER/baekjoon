7d8957dd96e4902d7f63a168da0e815eb0dc4416
ghnam <rhwkd11@gmail.com>
2021-03-14 23:52:05 +0900

    Input/Output/Calculations

---
+++ src/main/kotlin/backjoon/Q2557.kt
@@ -0,0 +1,21 @@
+package backjoon
+
+/**
+ * 문제
+ * Hello World!를 출력하시오.
+ *
+ * 입력
+ * 없음.
+ *
+ * 출력
+ * Hello World!를 출력하시오.
+ *
+ * 예제 입력
+ * 없음.
+ *
+ * 예제 출력
+ * Hello World!
+ */
+fun main(args: Array<String>) {
+    print("Hello World!")
+}
